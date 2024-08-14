a093ab4711c771424749561c7d1564a3a08dbdee
kushiamara <154491098+kushiamara@users.noreply.github.com>
2024-08-14 15:41:15 -0400

    edits to home page

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,9 +34,9 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('The Profs App')
+st.title('NomNetwork')
 st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.write('### Hi! Which user type would ?')
 
 # For each of the user personas for which we are implementing
 # functionality, we put a button on the screen that the user 
