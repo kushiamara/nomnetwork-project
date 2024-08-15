c131aecc4c6cbed32613501376b031a999822181
kushiamara <154491098+kushiamara@users.noreply.github.com>
2024-08-15 14:58:47 -0400

    post new review changes

--- app/src/pages/13_Post_New_Review.py
+++ app/src/pages/13_Post_New_Review.py
@@ -15,45 +15,43 @@ st.title('Post a New Review!')
 author = requests.get('http://api:4000/d/diner/author/emilyThompson').json()
 
 data = requests.get('http://api:4000/d/diner/restaurants').json()
+
 # Create a dictionary for dropdown options with names as keys and IDs as values
 options = {"Select a restaurant": None}
 options.update({restaurant['Name']: restaurant['ID'] for restaurant in data})
 
 # Create a dropdown in Streamlit
-selected_name = st.selectbox('Where did you dine?:', list(options.keys()))
+selected_name = st.selectbox('Where did you dine?', list(options.keys()))
 
 # Get the corresponding ID for the selected restaurant
 selected_id = options[selected_name]
 
-# create a 2 column layout
-# col1, col2 = st.columns(2)
-
-# add one number input for variable 1 into column 1
-# with col1:
-var_rating = st.number_input('Rating:',
+var_rating = st.number_input('Rate your experience from 1 to 5',
                            step=0.1)
 
-# add another number input for variable 2 into column 2
-# with col2:
-  
 # Create a multi-line text input box
 user_review = st.text_area(
-    "Enter your review here:",  # Label for the text area
+    "Share your thoughts",  # Label for the text area
     value="",  # Default value (can be an empty string)
     height=200  # Height of the text area in pixels
 )
 
-# logger.info(f'var_01 = {var_01}')
-# logger.info(f'var_02 = {var_02}')
 
-# add a button to use the values entered into the number field to send to the 
-# prediction function via the REST API
+# Create a multi-line text input box
+review_photo = st.text_area(
+    "Enter your photo link",  # Label for the text area
+    value="",  # Default value (can be an empty string)
+    height=5  # Height of the text area in pixels
+)
+
+
 # Construct the request payload
 request_payload = {
     "rating": var_rating,
     "text": user_review,
     "authorId": author["userId"],
-    "restId": selected_id
+    "restId": selected_id,
+    "photo": review_photo
 }
 
 # Create a button and handle its click event
