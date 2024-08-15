7973849fbe96023bd17fa4ec0ab1c9c9e110e8ae
kushiamara <154491098+kushiamara@users.noreply.github.com>
2024-08-15 14:24:49 -0400

    restaurant home page

--- app/src/pages/20_Restaurant_Home.py
+++ app/src/pages/20_Restaurant_Home.py
@@ -9,7 +9,10 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Restaurant Manager Home Page')
+st.title(f"Welcome Restaurant Manager, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
 
 if st.button('Edit My Menu', 
              type='primary',
