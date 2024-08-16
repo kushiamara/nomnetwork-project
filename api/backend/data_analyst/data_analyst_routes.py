9e42540524f650e25d74947416a2f8abcf2a7ec5
elliewilliams407 <williams.dani@northeastern.edu>
2024-08-15 23:23:22 -0400

    last update for the night

--- api/backend/data_analyst/data_analyst_routes.py
+++ api/backend/data_analyst/data_analyst_routes.py
@@ -62,10 +62,11 @@ def get_reviews():
 def get_reviews_high():
     current_app.logger.info('data_analyst_routes.py: GET /data_analyst/behavior/high')
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT u.username, r.reviewId, COUNT(timeViewed) AS NumberOfViews \
-        FROM Reviews r LEFT JOIN ReviewViews rv ON r.reviewId = rv.reviewId JOIN Users u on u.userId=r.authorId\
-        GROUP BY u.username, r.reviewId \
-        ORDER BY NumberOfViews desc ; ')
+    cursor.execute('SELECT u.username, r.reviewId, COUNT(rv.timeViewed) AS NumberOfViews, COUNT(c.commentId) AS NumberOfComments \
+        FROM Reviews r LEFT JOIN ReviewViews rv on r.reviewId = rv.reviewId LEFT JOIN Comments c on r.reviewId = c.reviewID JOIN Users u on u.userId=r.authorId\
+        GROUP BY reviewId \
+        ORDER BY NumberOfViews desc, NumberOfComments desc \
+        LIMIT 15; ')
     theData = cursor.fetchall()
     the_response = make_response(theData)
     the_response.status_code = 200
@@ -78,7 +79,7 @@ def get_reviews_low():
     current_app.logger.info('data_analyst_routes.py: GET /data_analyst/behavior/low')
     cursor = db.get_db().cursor()
     cursor.execute('SELECT u.username, r.reviewId, COUNT(rv.timeViewed) AS NumberOfViews, COUNT(c.commentId) AS NumberOfComments \
-        FROM Reviews r  JOIN ReviewViews rv on r.reviewId = rv.reviewId  JOIN Comments c on r.reviewId = c.reviewID JOIN Users u on u.userId=r.authorId\
+        FROM Reviews r LEFT JOIN ReviewViews rv on r.reviewId = rv.reviewId LEFT JOIN Comments c on r.reviewId = c.reviewID JOIN Users u on u.userId=r.authorId\
         GROUP BY reviewId \
         ORDER BY NumberOfViews desc, NumberOfComments asc \
         LIMIT 15; ')
@@ -110,7 +111,7 @@ def get_users():
     current_app.logger.info('data_analyst_routes.py: GET /data_analyst/users')
     cursor = db.get_db().cursor()
     cursor.execute('SELECT restName as RestaurantName, ROUND(AVG(DATEDIFF(CURRENT_DATE, u.dob) / 365)) as AvgDinerAge, \
-        SUM(CASE WHEN r.city != u.city THEN 1 ELSE 0 END) as numNotLocalDiners \
+        SUM(CASE WHEN r.city != u.city THEN 1 ELSE 0 END) as NumberOfNotLocalDiners \
             FROM Restaurants r LEFT JOIN Reviews rv ON r.restID = rv.restId LEFT JOIN Users u ON rv.authorId = u.userId \
             GROUP BY restName; ')
     theData = cursor.fetchall()
